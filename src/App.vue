d1d58f1331f62656b26e255e1758202737c04408
Danila Anikin <danila.s.anikin@gmail.com>
2022-11-02 17:59:19 +0100

    resolved conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-<<<<<<< HEAD
   import {createApp} from 'vue'
   import Squares from "./components/Squares.vue"
 
@@ -26,35 +25,4 @@ Gej -->
 
 <style scoped>
 
-=======
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
->>>>>>> 10af24bc37587a1f3619b40780ee18ad8b87b140
 </style>
